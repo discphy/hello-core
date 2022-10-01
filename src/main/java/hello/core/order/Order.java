87d71e30dbb3fe2dfd97286cb51adac785b31ad9
discphy <dinophy@nate.com>
2022-10-01 18:54:43 +0900

    feat(domain & service): Add Order & Discount

---
+++ src/main/java/hello/core/order/Order.java
@@ -0,0 +1,62 @@
+package hello.core.order;
+
+public class Order {
+
+    private Long id;
+    private String itemName;
+    private int itemPrice;
+    private int discountPrice;
+
+    public Order(Long id, String itemName, int itemPrice, int discountPrice) {
+        this.id = id;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.discountPrice = discountPrice;
+    }
+
+    public int calculatePrice() {
+        return itemPrice - discountPrice;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(int itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public int getDiscountPrice() {
+        return discountPrice;
+    }
+
+    public void setDiscountPrice(int discountPrice) {
+        this.discountPrice = discountPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", itemName='" + itemName + '\'' +
+                ", itemPrice=" + itemPrice +
+                ", discountPrice=" + discountPrice +
+                '}';
+    }
+}
