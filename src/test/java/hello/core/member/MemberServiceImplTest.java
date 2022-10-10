5e899b6ce0e8904e8a6329d39b64a55aa0b35cbd
discphy <dinophy@nate.com>
2022-10-10 22:44:13 +0900

    refactor(update): AppConfig

--- src/test/java/hello/core/member/MemberServiceImplTest.java
+++ src/test/java/hello/core/member/MemberServiceImplTest.java
@@ -1,14 +1,21 @@
 package hello.core.member;
 
-import org.assertj.core.api.Assertions;
+import hello.core.AppConfig;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceImplTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join() {
