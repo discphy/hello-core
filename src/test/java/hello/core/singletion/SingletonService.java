20cc35869170e6acd1cc8fc9ab3f12ebaf112051
discphy <dinophy@nate.com>
2022-10-12 00:01:09 +0900

    test(add): Singleton

--- src/test/java/hello/core/singletion/SingletonService.java
+++ src/test/java/hello/core/singletion/SingletonService.java
@@ -9,16 +9,9 @@ public class SingletonService {
     }
 
     private SingletonService() {
-
     }
 
     public void logic() {
         System.out.println("싱글톤 객체 로직 호출");
     }
-
-    public static void main(String[] args) {
-        SingletonService singletonService1 = new SingletonService();
-        SingletonService singletonService2 = new SingletonService();
-    }
-
 }
--- src/test/java/hello/core/singletion/SingletonTest.java
+++ src/test/java/hello/core/singletion/SingletonTest.java
@@ -4,6 +4,8 @@ import hello.core.AppConfig;
 import hello.core.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,5 +23,31 @@ public class SingletonTest {
         System.out.println("memberService2 = " + memberService2);
 
         assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너 싱글톤")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
