90af3bd9f7708a7dab2b090ddc47e07896e9450f
discphy <dinophy@nate.com>
2022-10-23 13:51:54 +0900

    feat(add): Bean lifecycle callback

---
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,29 @@
+package hello.core.lifecycle;
+
+public class NetworkClient {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("url = " + url);
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void connect() {
+        System.out.println("connect : " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call : " + url);
+        System.out.println("message = " + message);
+    }
+
+    public void disconnect() {
+        System.out.println("close : " + url);
+    }
+}
